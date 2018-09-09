9b410fad09f3c73358f63e4fa43ac952214ca4c0
Mykola Marzhan <mykola.marzhan@percona.com>
2018-09-09 15:22:53 +0300

    rework tags

--- eks-pxc/worker-main.tf
+++ eks-pxc/worker-main.tf
@@ -28,21 +28,9 @@ resource "aws_autoscaling_group" "worker" {
   min_size             = 2
   vpc_zone_identifier  = ["${module.vpc.public_subnets}"]
 
-  tag {
-    key                 = "Name"
-    value               = "${var.cluster-name}"
-    propagate_at_launch = true
-  }
-
-  tag {
-    key                 = "iit-billing-tag"
-    value               = "${var.cluster-name}"
-    propagate_at_launch = true
-  }
-
-  tag {
-    key                 = "kubernetes.io/cluster/${var.cluster-name}"
-    value               = "owned"
-    propagate_at_launch = true
-  }
+  tags = ["${list(
+      map("key", "Name", "value", "${var.cluster-name}-worker", "propagate_at_launch", true),
+      map("key", "iit-billing-tag", "value", "${var.cluster-name}", "propagate_at_launch", true),
+      map("key", "kubernetes.io/cluster/${var.cluster-name}", "value", "owned", "propagate_at_launch", true)
+  )}"]
 }
