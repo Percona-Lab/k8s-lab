a309dc4215d4b33ff73ac870db4492cabb1a0c13
Mykola Marzhan <mykola.marzhan@percona.com>
2020-04-09 16:45:27 +0300

    PBM-450 tune defaults

--- go-s3-upload/s3gof3r-lib/s3gof3r.go
+++ go-s3-upload/s3gof3r-lib/s3gof3r.go
@@ -17,8 +17,8 @@ func main() {
 	secretKey := os.Args[3]
 	bucket := os.Args[4]
 	dest := os.Args[5]
-	partSize := 10 * 1024 * 1024
-	threads := runtime.NumCPU()
+	partSize := 128 * 1024 * 1024
+	threads := int(float64(runtime.NumCPU()) * 1.5)
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Starting upload")
 
